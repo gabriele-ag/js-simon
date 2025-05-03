d36d42c0bb0f349f62ebb6c160ec773708bb2674
gabriele-ag <gabrieleardizzone.gdesigner@gmail.com>
2025-05-03 14:40:44 +0200

    feat: button start game done init confirmed button

--- js/script.js
+++ js/script.js
@@ -7,19 +7,29 @@ const inputElem2 = document.getElementById('casella2');
 const inputElem3 = document.getElementById('casella3');
 const inputElem4 = document.getElementById('casella4');
 const inputElem5 = document.getElementById('casella5');
-const button = document.getElementById('inizia');
+const buttonStart = document.getElementById('inizia');
+const buttonConfirmed = document.getElementById('conferma')
 const result = document.getElementById('risultato');
+const elemInputPlayer = document.getElementById('inputGiocatore');
 
 
-const playerNumber1 = inputElem1.value;
-const playerNumber2 = inputElem2.value;
-const playerNumber3 = inputElem3.value;
-const playerNumber4 = inputElem4.value;
-const playerNumber5 = inputElem5.value;
+const firstNumber = inputElem1.value;
+const secondNumber = inputElem2.value;
+const thirdNumber = inputElem3.value;
+const fourthNumber = inputElem4.value;
+const fifthNumber = inputElem5.value;
 
 
-// Definisco array di 5 num casuali
-button.addEventListener('click', startGame)
+// Nascondo gli input del giocatore
+elemInputPlayer.style.display = "none"
+buttonConfirmed.style.display = "none"
+
+// Array di 5 numeri casuali
+const randomNumber = []
+
+
+// Creo evento per la creazioni di 5 numeri casuali
+buttonStart.addEventListener('click', startGame)
 
 
 function startGame(event) {
@@ -28,17 +38,14 @@ function startGame(event) {
     event.preventDefault();
     console.log("Inizia il gioco")
 
-    const tempo = timer.value
-
     // Creo 5 numeri casuali
-    const randomNumber = []
     
     for (let i = 0; i < 5; i++) {
        randomNumber.push(parseInt(Math.floor(Math.random() * 99) + 1))
         console.log(randomNumber)
     }
-
-
+    
+    
     // Creo il timer
 
     let count = 30;
@@ -47,26 +54,41 @@ function startGame(event) {
         if (count === 0) {
             clearInterval(intervalID)
             console.log("Tempo scaduto!")
+            timer.innerHTML = "Scrivi i numeri che ricordi!"
+            elemNum1.innerHTML = ""
+            elemInputPlayer.style.display = "block"
+            buttonConfirmed.style.display = "block"
+            buttonStart.style.display = "none"
         } else {
             count--;
+            timer.innerHTML = count;
             console.log(count)
         }
     }, 1000);
 
-    // let count = 30;
 
-    // const intervalId = setInterval(function () {
-    // if (count === 0) {
-    // console.log("Buon Anno!!!");
-    // clearInterval(intervalId);
-    // } else {
-    // console.log(count);
-    // count--;
-    // }
-    // }, 1000);
-    timer.innerHTML = intervalID
-    elemNum1.innerHTML = randomNumber
-    elemNum1.reset
-    timer.reset
+    elemNum1.innerHTML = randomNumber.join(" ")
     
 }
+
+
+
+
+// Creo evento per confronto risposte del giocatore e numeri dati dal gioco
+
+
+buttonConfirmed.addEventListener('click', endGame)
+
+function endGame(event) {
+    // Evito che la pagina faccia refresh
+    event.preventDefault();
+    console.log("Fine gioco!")
+
+    if ()
+}
+
+
+
+
+
+
