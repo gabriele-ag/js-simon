4ae596197b1b22a9fdc7eed47bc94d7459f192e1
gabriele-ag <gabrieleardizzone.gdesigner@gmail.com>
2025-05-03 10:48:26 +0200

    feat: init project

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Estraggo gli elementi che mi servono
+
+const elemNum1 = document.getElementById('num1');
+const elemNum2 = document.getElementById('num2');
+const elemNum3 = document.getElementById('num3');
+const elemNum4 = document.getElementById('num4');
+const elemNum5 = document.getElementById('num5');
+const inputElem1 = document.getElementById('casella1');
+const inputElem2 = document.getElementById('casella2');
+const inputElem3 = document.getElementById('casella3');
+const inputElem4 = document.getElementById('casella4');
+const inputElem5 = document.getElementById('casella5');
+const button = document.getElementById('inizia');
+const result = document.getElementById('risultato');
+
+
+
+
