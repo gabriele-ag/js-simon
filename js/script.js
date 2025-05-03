9ae7a15a8dc29de78bdfba4b84f4b0654b808d73
gabriele-ag <gabrieleardizzone.gdesigner@gmail.com>
2025-05-03 15:45:33 +0200

    feat: game created

--- js/script.js
+++ js/script.js
@@ -1,31 +1,36 @@
 // Estraggo gli elementi che mi servono
 
-const elemNum1 = document.getElementById('num1');
+// Numeri casuali da generare e timer
+const elemRandom = document.getElementById('numRandom');
 const timer = document.getElementById('contatore');
+
+// InputGiocatore
 const inputElem1 = document.getElementById('casella1');
 const inputElem2 = document.getElementById('casella2');
 const inputElem3 = document.getElementById('casella3');
 const inputElem4 = document.getElementById('casella4');
 const inputElem5 = document.getElementById('casella5');
+
+// Bottoni
 const buttonStart = document.getElementById('inizia');
+const buttonReset = document.getElementById('reset')
 const buttonConfirmed = document.getElementById('conferma')
-const result = document.getElementById('risultato');
+
+// div InputGiocatore da nascondere
 const elemInputPlayer = document.getElementById('inputGiocatore');
 
-
-const firstNumber = inputElem1.value;
-const secondNumber = inputElem2.value;
-const thirdNumber = inputElem3.value;
-const fourthNumber = inputElem4.value;
-const fifthNumber = inputElem5.value;
+// Risultato
+const result = document.getElementById('risultato');
 
 
-// Nascondo gli input del giocatore
+
+// Nascondo gli input del giocatore e il bottone Conferma
 elemInputPlayer.style.display = "none"
 buttonConfirmed.style.display = "none"
 
+
 // Array di 5 numeri casuali
-const randomNumber = []
+const randomNumbers = []
 
 
 // Creo evento per la creazioni di 5 numeri casuali
@@ -41,8 +46,8 @@ function startGame(event) {
     // Creo 5 numeri casuali
     
     for (let i = 0; i < 5; i++) {
-       randomNumber.push(parseInt(Math.floor(Math.random() * 99) + 1))
-        console.log(randomNumber)
+       randomNumbers.push(parseInt(Math.floor(Math.random() * 99) + 1))
+        console.log(randomNumbers)
     }
     
     
@@ -55,7 +60,7 @@ function startGame(event) {
             clearInterval(intervalID)
             console.log("Tempo scaduto!")
             timer.innerHTML = "Scrivi i numeri che ricordi!"
-            elemNum1.innerHTML = ""
+            elemRandom.innerHTML = ""
             elemInputPlayer.style.display = "block"
             buttonConfirmed.style.display = "block"
             buttonStart.style.display = "none"
@@ -67,7 +72,7 @@ function startGame(event) {
     }, 1000);
 
 
-    elemNum1.innerHTML = randomNumber.join(" ")
+    elemRandom.innerHTML = randomNumbers.join(" ")
     
 }
 
@@ -84,8 +89,56 @@ function endGame(event) {
     event.preventDefault();
     console.log("Fine gioco!")
 
-    if ()
+    // Preparo gli input del giocatore
+    const firstNumber = inputElem1.value;
+    const secondNumber = inputElem2.value;
+    const thirdNumber = inputElem3.value;
+    const fourthNumber = inputElem4.value;
+    const fifthNumber = inputElem5.value;
+
+    // Definisco condizione per il gli input del giocatore
+    const playerNumbers = [firstNumber, secondNumber, thirdNumber, fourthNumber, fifthNumber]
+    let correct = 0
+
+    for (let i = 0; i < randomNumbers.length; i++) {
+        if (playerNumbers.includes(String(randomNumbers[i]))) {
+            correct++
+        }
+    }
+    
+
+    // Stampo risultato finale. Vediamo quanto ne ha indovinati il giocatore!
+    console.log(`Hai indovinato ${correct} numeri!`)
+    result.innerHTML = `Hai indovinato ${correct} numeri!`
+
 }
+
+
+// Creo Reset del gioco
+
+
+buttonReset.addEventListener('click', resetGame)
+
+function resetGame(event) {
+    event.preventDefault();
+
+    // Resetto il gioco
+    inputElem1.value = "";
+    inputElem2.value = "";
+    inputElem3.value = "";
+    inputElem4.value = "";
+    inputElem5.value = "";
+    result.innerHTML = "";
+    elemRandom.innerHTML = "";
+    timer.innerHTML = "30";
+    elemInputPlayer.style.display = "none";
+    buttonConfirmed.style.display = "none";
+    buttonStart.style.display = "block";
+    randomNumbers.length = 0
+
+}
+
+
 
 
 
